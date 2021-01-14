fc083a727bcbbdf91832e661532e845777eb9550
ehdavison <elijahdavison2@gmail.com>
2021-01-14 11:15:57 -0600

    detail and index set up

---
+++ components/Detail.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
+
+
+const Detail = () => {
+    return (
+        <View>
+            <ScrollView>
+                <Text>Hello Bitches DETAIL VIEW</Text>
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Detail
---
+++ components/Index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
+
+
+const Index = () => {
+    return (
+        <View>
+            <ScrollView>
+                <Text>Hello Bitches</Text>
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Index
