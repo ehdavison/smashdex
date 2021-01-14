9f032b82901513b75b92d667bb4bd1b983f3fe3a
ehdavison <elijahdavison2@gmail.com>
2021-01-14 11:23:24 -0600

    components are ready for styling

--- App.js
+++ App.js
@@ -24,20 +24,27 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+import Detail from "./components/Detail"
+import Index from "./components/Index"
+
+const App = () => {
   return (
-      <View>
-        <ScrollView>
-          <View>
-            <Text>Hello World-ya mama</Text>
-          </View>
-        </ScrollView>
+      <View style={styles.container}>
+        {/* <Detail />
+        <Index /> */}
+        <Text>HELLO</Text>
       </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    marginTop: 200,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
 
 export default App;
