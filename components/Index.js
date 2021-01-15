0708d74242ff6faeecc73a0b6f0655b1ab257cc2
ehdavison <elijahdavison2@gmail.com>
2021-01-15 09:44:17 -0600

    pictures are now dynamic

--- components/Index.js
+++ components/Index.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import {View, Text, ScrollView, StyleSheet, Button, Image} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Button,
+  Image,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
 import Characters from '../api/characters.json';
 import Images from '../api/images/Images';
 
 const Index = () => {
   const mappedCharacters = Characters.characters;
+  const fuck = () => {
+    Alert.alert('fuck');
+  };
   return (
     <View style={styles.container}>
-      <Button title="hello" onPress={console} />
+      <Text>SmashDex</Text>
       {mappedCharacters.map((character, index) => {
         let path = Images[character.name.toLowerCase()];
         console.log(character.name.toLowerCase());
         return (
           <View style={styles.characters} key={index}>
-            <Text>{character.name}</Text>
-            <Image source={path} />
+            <TouchableOpacity onPress={fuck}>
+              <Text>{character.name}</Text>
+              <Image source={path} />
+            </TouchableOpacity>
           </View>
         );
       })}
@@ -24,13 +38,21 @@ const Index = () => {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 150,
+    marginTop: 100,
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
   },
   characters: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.8,
+    elevation: 5,
     margin: 5,
     borderColor: 'black',
     borderRadius: 14,
